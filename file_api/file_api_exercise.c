cdd31f7ccaaa6407ffec38fc0a2edef339d076bf
MasahiroNakata <garois0812@gmail.com>
2020-09-20 02:44:02 +0900

    addition of error handler by errono

--- file_api/file_api_exercise.c
+++ file_api/file_api_exercise.c
@@ -2,6 +2,8 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <stdio.h>
+#include <string.h>
+#include <errno.h>
 
 #define BUF_SIZE 11
 
@@ -9,16 +11,27 @@
 int main(int argc, char *argv[])
 {
   char buf[BUF_SIZE];
+  int number;
 
   int inputFd = open (argv[1], O_RDONLY);
+  number = errno;
 
-  read(inputFd, buf, BUF_SIZE);
+  if(inputFd == -1)
+  {
+    perror("file open error string by perror!");
+    printf("file open error string by strerror: %s\n", strerror(number));
+
+    printf("print error code: %d\n", number);
+  }
+  else
+  {
+    read(inputFd, buf, BUF_SIZE);
  
-  close(inputFd);
+    close(inputFd);
 
-  for (unsigned int i = 0; i < BUF_SIZE; i++)
-    printf("%c", buf[i]);
-
+    for (unsigned int i = 0; i < BUF_SIZE; i++)
+      printf("%c", buf[i]);
+  }
   return 0;
 
 }
